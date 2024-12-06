126af45ca7ffaa586334168a058c6b6a52a6afbe
HoangNV120 <nguyenviethoang12022003@gmail.com>
2024-12-06 11:08:22 +0700

    Initial Project

---
+++ fpt-game-booking-be/src/main/java/com/fptgamebookingbe/FptGameBookingBeApplication.java
@@ -0,0 +1,13 @@
+package com.fptgamebookingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FptGameBookingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FptGameBookingBeApplication.class, args);
+    }
+
+}
